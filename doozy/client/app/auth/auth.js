ee188c5f2f5c2c61c2614bf717600fb7d525904e
Jeff Cousins <jeffcousins@me.com>
2015-11-17 14:19:23 -0600

    updated rebase

--- doozy/client/app/auth/auth.js
+++ doozy/client/app/auth/auth.js
@@ -1,27 +1,27 @@
 angular.module('app.auth', [])
 
-.controller('AuthController', function($scope, $http, $location, Auth) {
+.controller('AuthController', function($scope, $http) {
   $scope.user = {};
 
-  $scope.signin = function() {
-    Auth.signin($scope.user)
-      .then(function(token) {
-        $window.localStorage.setItem('wat', token);
-        $location.path('/tasks');
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
+  $scope.signin = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/login',
+      data: user
+    })
+    .then(function(response) {
+      return response.data;
+    });
   };
 
-  $scope.signup = function() {
-    Auth.signup($scope.user)
-      .then(function(token) {
-        $window.localStorage.setItem('wat', token);
-        $location.path('/tasks');
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
+  $scope.signup = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/signup',
+      data: user
+    }).then(function(response) {
+      return response.data;
+    });
   };
+
 });
