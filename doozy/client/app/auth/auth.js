fe8f97528577e0f89f8b40ed4d307d97bc38ea72
Jeff Cousins <jeffcousins@me.com>
2015-11-17 21:39:44 -0600

    auth revamp

--- doozy/client/app/auth/auth.js
+++ doozy/client/app/auth/auth.js
@@ -1,63 +1,27 @@
 angular.module('app.auth', [])
 
-.controller('AuthController', function($scope, $http, TokenFactory) {
+.controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
 
-  $scope.signin = function(user) {
-    return $http({
-      method: 'POST',
-      url: '/api/login',
-      data: user
-    })
-    .then(function(response) {
-      return response.data;
-    });
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('auth-token', token);
+        $location.path('/tasks');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
 
-  $scope.signup = function(user) {
-    return $http({
-      method: 'POST',
-      url: '/api/signup',
-      data: user
-    }).then(function(response) {
-      TokenFactory.setToken(response.data.token);
-      return response;
-    });
-  };
-})
-
-.factory('TokenFactory', function($window) {
-  var storage = $window.localStorage;
-  var key = 'auth-token';
-
-  var getToken = function() {
-    return storage.getItem(key);
-  };
-
-  var setToken = function(token) {
-    if (token) {
-      storage.setItem(key, token);
-    } else {
-      store.removeItem(key);
-    }
-  };
-
-  return {
-    getToken: getToken,
-    setToken: setToken
-  };
-})
-.factory('AuthInterceptor', function(TokenFactory) {
-  var addToken = function(config) {
-    var token = TokenFactory.getToken();
-    if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = 'Bearer ' + token;
-    }
-    return config;
-  };
-
-  return {
-    request: addToken
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('auth-token', token);
+        $location.path('/tasks');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
 });
