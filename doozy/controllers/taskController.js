8d2568ad8dcc041a19c1d3c34bf4e79f424bad3f
Zachary R. Smith <ZacharySmith4989@gmail.com>
2015-11-19 17:39:56 -0500

    (doc) Add description of back-end structure to README

--- doozy/controllers/taskController.js
+++ doozy/controllers/taskController.js
@@ -68,8 +68,8 @@ module.exports = {
       task.description = req.body.description;
       task.isCompleted = req.body.isCompleted;
       task.users = req.body.users;
-      console.log('users: ')
-      console.log(req.body.users);
+      // console.log('users: ');
+      // console.log(req.body.users);
 
       task.save(function (err, task) {
         if (err) console.log('err: ' , err);
@@ -81,11 +81,11 @@ module.exports = {
   },
   deleteTask: function(req, res, next) {
     var taskId = mongoose.Types.ObjectId(req.params.id);
-    console.log('delete task: ' + taskId);
+    // console.log('delete task: ' + taskId);
     Task.remove({ _id: taskId }, function(err) {
-      if (err) return res.sendStatus(404, err)
+      if (err) return res.sendStatus(404, err);
       
       res.sendStatus(205);
-    })
+    });
   }
 };
