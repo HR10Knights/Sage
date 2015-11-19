58050a58bbb016561f81ea015feb89db40625733
apedley <apedley@gmail.com>
2015-11-19 13:42:35 -0600

    Fixed backend empty description bug

--- doozy/config.js
+++ doozy/config.js
@@ -85,12 +85,6 @@ db.projectsSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    validate: [
-      function(description) {
-        return description.trim().length >= 3;
-      },
-      'Description too short'
-    ]
   },
   created_at: Date,
   updated_at: Date
