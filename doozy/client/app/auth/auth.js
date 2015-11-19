5c4ed58b4f1f42205c360303eeac0e28ed811cf0
Jeff Cousins <jeffcousins@me.com>
2015-11-19 08:33:20 -0600

    signin/signup layout. added error alert on failed signin

--- doozy/client/app/auth/auth.js
+++ doozy/client/app/auth/auth.js
@@ -14,6 +14,7 @@ angular.module('app.auth', [])
         $location.path('/tasks');
       })
       .catch(function (error) {
+        alert(error.data);
         console.error(error);
       });
   };
@@ -25,6 +26,7 @@ angular.module('app.auth', [])
         $location.path('/tasks');
       })
       .catch(function (error) {
+        alert(error.data);
         console.error(error);
       });
   };
