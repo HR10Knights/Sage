46b7205fb00e8258ea2cbb1eab635b1ddcc14f15
Zachary R. Smith <ZacharySmith4989@gmail.com>
2015-11-19 20:15:56 -0500

    Update _ROUTES.md

--- doozy/routes/tasks.js
+++ doozy/routes/tasks.js
@@ -7,23 +7,22 @@ var taskController = require('../controllers/taskController');
 // List tasks
 router.get('/', taskController.allTasks);
 
+// Create task
+// curl -H "Content-Type: application/json" -X POST -d '{"name":"my task 2","description":"my description"}' http://localhost:3000/api/tasks
+router.post('/', taskController.createTask);
+
 // View task
 router.get('/:id', taskController.idToTask);
 
 // Update task
 router.put('/:id', taskController.updateTask);
 
-// Create task
-// curl -H "Content-Type: application/json" -X POST -d '{"name":"my task 2","description":"my description"}' http://localhost:3000/api/tasks
-router.post('/', taskController.createTask);
-
+// Delete task
+router.delete('/:id', taskController.deleteTask);
 
 // Assign task to user
 // curl -H "Content-Type: application/json" -X POST -d '{"user":"564cd72524d8b619223cd11b","task":"564d0970c7de37cd2bdb3ec2"}' http://localhost:3000/api/tasks/assign
 router.post('/assign', taskController.assignTask);
-
-// Delete task
-router.delete('/:id', taskController.deleteTask);
 
 
 module.exports = router;
