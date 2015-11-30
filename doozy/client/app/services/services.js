0ccbe65ae9c2ed2f82f8de27ee19afd1845d14b3
Luke Powell <lucasbpowell@gmail.com>
2015-11-30 11:37:35 -0500

    added comments for auth code in services.js

--- doozy/client/app/services/services.js
+++ doozy/client/app/services/services.js
@@ -75,12 +75,14 @@ optional properties: users, description
 .factory('Auth', function ($http, $location, $window) {
   var teamName = '';
 
+  // sends user login input to db
   var signin = function (user) {
     return $http({
       method: 'POST',
       url: '/api/login',
       data: user
     })
+    // if successful, send encoded token back to auth.js
     .then(function (resp) {
       teamName = user.teamname;
       return resp.data.token;
@@ -93,16 +95,20 @@ optional properties: users, description
       url: '/api/signup',
       data: user
     })
+    // if successful, send encoded token back to auth.js
     .then(function (resp) {
       teamName = user.teamname;
       return resp.data.token;
     });
   };
 
+  // checks if user has a previously stored web token
+  // returning false will redirect user to signin page
   var isAuth = function () {
     return !!$window.localStorage.getItem('auth-token');
   };
 
+  // clears web token and redirect to signin
   var signout = function () {
     $window.localStorage.removeItem('auth-token');
     $location.path('/signin');
