ac5f50e6d248bf426430ade65a42aeb8237c3cd7
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-03 17:19:25 -0500

    (feat) Working on Organization View

---
+++ doozy/client/app/directives/organizationCard/organizationCard.js
@@ -0,0 +1,61 @@
+angular.module('directives.organizationCard', [])
+
+.directive('organizationCard', function(Project, Organization) {
+    return {
+      replace: true,
+      templateUrl: 'app/directives/organizationCard/organizationCard.html',
+      controller: 'OrganizationCardController',
+      scope: {
+        org: '=',
+      }
+    }
+  })
+  .controller('OrganizationCardController', ['$scope', 'Project', 'Organization', function($scope, Project, Organization) {
+    $scope.showProjectForm = false;
+    $scope.showProjectButton = true;
+    $scope.project = {};
+    $scope.data = {};
+    $scope.org.projects = [{
+      name: 'Project1'
+    }, {
+      name: 'Project2'
+    }];
+
+    Organization.getUserByOrganizationId($scope.org._id)
+    	.then(function(users){
+    		$scope.data.users = users;
+    	});
+
+    $scope.refreshOrganizationCard = function(){
+    	Organization.getOrganizationById()
+    		.then(function(org){
+    			$scope.org = org;
+    		});
+    };
+
+    $scope.newProject = function(data) {
+    	data.orgId = $scope.org._id;
+      Project.createProjectByOrgID(data)
+        .then(function() {
+          $scope.refreshOrganizationCard();
+        });
+    };
+
+    $scope.resetProjectDetails = function() {
+      // reset the task form if it was dirty
+      if ($scope.project) {
+        $scope.project.name = null;
+        $scope.project.users = null;
+        // reset the form validation
+        $scope.projectForm.$setUntouched();
+      }
+
+      // hide the task form
+      $scope.showProjectForm = false;
+      // show the 'Add Task' button
+      $scope.showProjectButton = true;
+    };
+
+
+
+  }]);
--- doozy/models/task.js
+++ doozy/models/task.js
@@ -1,5 +1,7 @@
 var db = require('../config');
 var mongoose = require('mongoose');
+var User = require('./user');
+var Project = require('./project');
 
 // db.tasksSchema.pre('init', function(next, data) {
 //   Task.populate(data, {
@@ -11,17 +13,24 @@ var mongoose = require('mongoose');
 // });
 
 db.tasksSchema.pre('remove', function(next, done) {
-  User.update({}, {$pull: {task_list: this._id}}, function (err, user){
-    if (err) {
-      console.log(err);
-    }
-  });
-  
-  Project.update({}, {$pull: {tasks: this._id}}, function (err, project){
-    if (err) {
-      console.log(err);
-    }
-  });
+  User.update({}, {
+      $pull: {
+        task_list: this._id
+      }
+    })
+    .then(function() {
+
+      Project.update({}, {
+        $pull: {
+          tasks: this._id
+        }
+      }, function(err, project) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+
 });
 
 var Task = mongoose.model('Task', db.tasksSchema);
