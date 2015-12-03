5ff59cca108d0316cc0bd741cc9fcfe895b01673
mgaff <mgaffney123@gmail.com>
2015-12-03 14:00:52 -0500

    (feat) completed organization and project factories

--- doozy/client/app/services/organization.js
+++ doozy/client/app/services/organization.js
@@ -1,18 +1,69 @@
 angular.module('services.organization', [])
 
-.factory('Organization', ['$http', function($http) {
+  .factory('Organization', function ($http) {
 
-  return {
-    createOrganization: function(org) {
+    var allOrganizations = function (){
       return $http({
-        method: 'POST',
-        url: 'api/org',
-        data: {
-          org: org
-        }
-      }).then(function(resp) {
+        method: 'GET',
+        url: 'api/orgs/'
+      }).then(function (resp) {
         return resp.data;
       });
+    };
+
+    var getOrganizationById = function (orgId){
+      return $http({
+        method: 'GET',
+        url: 'api/orgs/' + orgId
+      }).then(function (resp){
+        return resp.data;
+      });
+    };
+
+    var getUserByOrganizationId = function(orgId){
+      return $http({
+        method: 'GET',
+        url: 'api/orgs/users' + orgId
+      }).then(function (resp){
+        return resp.data;
+      });
+    };
+
+    var createOrganization = function (newOrg){
+      return $http({
+        method: 'POST',
+        url: 'api/orgs/',
+        data: newOrg
+      }).then(function (resp){
+        return resp.data;
+      });
+    };
+
+    var updateOrganization = function (newData){
+      return $http({
+        method: 'PUT',
+        url: 'api/orgs',
+        data: newData
+      }).then(function (resp){
+        return resp.data;
+      });
+    };
+
+    var removeOrganization = function (orgId){
+      return $http({
+        method: 'DELETE'
+        url: 'api/orgs' + orgId
+      }).then(function (resp) {
+        return resp.data;
+      });
+    };
+
+    return {
+      allOrganizations: allOrganizations,
+      getOrganizationById: getOrganizationById,
+      getUserByOrganizationId: getUserByOrganizationId,
+      createOrganization: createOrganization,
+      updateOrganization: updateOrganization,
+      removeOrganization: removeOrganization
     }
-  };
-}]);
+  });
---
+++ doozy/client/app/services/projects.js
@@ -0,0 +1,75 @@
+angular.module('services.project', [])
+
+  .factory('Project', function($http) {
+    /**
+     * Return all users currently assigned to a task
+     * @param  {[int]} taskId   [id of the given task]
+     * @return {[array]}        [array of user objects]
+     */
+    var createProjectByOrgID = function(orgID, project){
+      return $http({
+        method: 'POST',
+        url: 'api/projects/create',
+        data: project
+      }).then(function(resp) {
+        return resp.data;
+      });
+    };
+
+    var getProjectById = function(projectId) {
+      return $http({
+        method: 'GET',
+        url: 'api/projects/' + projectID
+      }).then(function (resp){
+        return resp.data;
+      });
+    };
+
+    var getUserByProjectId = function (projectId) {
+      return $http({
+        method: 'GET',
+        url: 'api/projects/users/' + projectId
+      }).then(function (resp){
+        return resp.data
+      });
+    };
+
+    var removeProject = function (projectId) {
+      return $http({
+        method: 'DELETE',
+        url: 'api/projects/' + projectId
+      }).then(function (resp){
+        return resp.data
+      });
+    };
+
+    var updateProject = function(projectId, newData){
+      return $http({
+        method: 'PUT',
+        url: 'api/projects',
+        data: newData
+      }).then(function (resp){
+        return resp.data
+      });
+    };
+
+    var allProjects = function(){
+      return $http({
+        method: 'GET',
+        url 'api/projects'
+      }).then(function (resp){
+        return resp.data
+      })
+    }
+
+    return {
+      createProjectByOrgID: createProjectByOrgID,
+      getProjectById: getProjectById,
+      getUserByProjectId: getUserByProjectId,
+      removeProject: removeProject,
+      updateProject: updateProject,
+      allProjects: allProjects
+    };
+  });
+
+
