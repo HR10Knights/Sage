775101bb1dd9c138ef43d9dd0dd197fee4ca5780
mgaff <mgaffney123@gmail.com>
2015-12-03 13:10:11 -0500

    (feat) removal cleanup

--- doozy/models/project.js
+++ doozy/models/project.js
@@ -1,12 +1,38 @@
 var db = require('../config');
 var mongoose = require('mongoose');
 
+
 db.projectsSchema.pre('init', function(next, data) {
   Project.populate(data, {
     path: 'tasks'
   }, function(err, project) {
     data = project;
     next();
+  });
+});
+
+db.projectsSchema.pre('remove', function(next, done) {
+  User.update({}, {$pull: {project_list: this._id}}, function (err, user){
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  Org.update({}, {$pull: {projects: this._id}}, function (err, org){
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  Task.find({project_id: this._id}, function (err, task){
+    if (err) {
+      console.log(err);
+    }
+    for (var i = 0; i < task.length; i++) {
+      if (task[i]) {
+        removeTask(task[i]);
+      }
+    }
   });
 });
 
