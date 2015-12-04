7f26b7f8ce518b59ffd20aa4eea5460375fc404f
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-03 20:06:43 -0500

    (feat) Can add users to projects.

---
+++ doozy/client/app/directives/projectCard/projectCard.js
@@ -0,0 +1,40 @@
+angular.module('directives.projectCard', [])
+
+.directive('projectCard', function(Users, Project, Organization) {
+    return {
+      replace: true,
+      templateUrl: 'app/directives/projectCard/projectCard.html',
+      controller: 'ProjectCardController',
+      scope: {
+        project: '=',
+        org: '='
+      }
+    };
+  })
+  .controller('ProjectCardController', ['$scope', 'Users', 'Project', 'Organization', function($scope, Users, Project, Organization) {
+    $scope.data = {};
+
+    $scope.updateUserList = function() {
+      Project.getUserByProjectId($scope.project._id)
+        .then(function(users) {
+          $scope.data.projectUsers = users;
+        });
+    };
+
+    Organization.getUserByOrganizationId($scope.org._id)
+      .then(function(users) {
+        $scope.data.orgUsers = users;
+      });
+
+    $scope.addUser = function(userId) {
+      Users.addProjectToUser({
+        userId: userId,
+        projectId: $scope.project._id
+      }).then(function() {
+        $scope.updateUserList();
+      });
+    };
+
+    $scope.updateUserList();
+
+  }]);
